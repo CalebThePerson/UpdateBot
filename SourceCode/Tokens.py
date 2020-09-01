f4af46b40fef131a1791529fcba4031dfda43c62
Caleb Wheeler <Cayj15@gmail.com>
2020-09-01 09:43:01 -0700

    ignore go brrr

--- SourceCode/Tokens.py
+++
@@ -1,5 +0,0 @@
-ConsumerKey = "ApgUQKisHtpfEoRU4TbU2nKAw"
-ConsumerSecret = "lCGv6faLitt6WscLVClXmFkRnrRdPCEq6GVhvGQfVxXz3enB1L"
-AccessToken = "1157357550871834624-q4HY1b0MosFLUM1BNRIWoOLeTXyFfh"
-SecretToken = "dIgyY7OFR1Y1nft62Pl7L4rx5puz941sauCVoHDNbNuC5"
-
