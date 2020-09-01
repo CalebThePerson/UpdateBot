627dabec48ab7d30ff04eadcf84a57d220edc378
Caleb Wheeler <Cayj15@gmail.com>
2020-08-31 20:31:32 -0700

    Before i go into unknown territory

--- SourceCode/api.py
+++ SourceCode/api.py
@@ -22,18 +22,73 @@ def LimitHandler(cursor):
 class TweetBot():
 
     def TweetingUpdate(self):
-        api.update_status("Commit #7")
-        pass
+        api.update_status("Pleae Help Me")
+        print("Done")
 
+    def Stuff(self,TheArray):
+        amount = len(TheArray)
+        NewArrayForChecking = []
+
+        for num in range(0,amount):
+
+            try:
+                if int(TheArray[num]):
+                    print("Good")
+                    print(TheArray[num])
+                    NewArrayForChecking.append(TheArray[num])
+
+            except ValueError:
+                print("Not poggers")
+                pass
+
+            pass
+        
+        NewArrayForChecking.sort()
+        # return NewArrayForChecking[0]
+            
+
+    def CharacterStruffRenameLater(self, TheStatus, yeth):
+        if len(TheStatus) > 8 and len(yeth) < 9:
+            print(TheStatus)
+            return TheStatus[:8]
+            
+        elif len(TheStatus) > 9 and len(yeth) < 10:
+            print(TheStatus)
+            return TheStatus[:9]
+            
+
+    def FilterFunction(self, TheTweet):
+        if filter(TheTweet, "Commit"):
+            return TheTweet
+        
 
     def CommitNumber(self):
-        for status in LimitHandler(tweepy.Cursor(api.user_timeline).items(2)):
-            x = status.text
-            pass
+        welp = []
+
+
+        for status in LimitHandler(tweepy.Cursor(api.user_timeline).items(7)):
+
+
+            x = str(status.text)
+            print(x)[:8]
+            
+            if filter(x,"Commit #"):
+
+                yeths = [i for ele in x for i in ele]
+                welp.append(self.CharacterStruffRenameLater(x, yeths))
+
+        print(welp)
+
+
+                # print(status.text)
+
+            # self.Stuff(welp)
+
 
     def BotherMakar(self):
         api.send_direct_message("958734505006608384", "Hey cutie")
 
 
 yeth = TweetBot()
+# yeth.TweetingUpdate()
 yeth.CommitNumber()
